f64b4344d70d975ee0f3948186d420c8edf7b450
froriz556 <alex_fomof@mail.ru>
2025-12-31 01:04:58 +0300

    rework db helper

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -6,7 +6,11 @@ from core.config import setting
 class DataBaseHelper:
     def __init__(self, url):
         self.engine = create_async_engine(url=url)
-        self.session_factory = async_sessionmaker(engine=self.engine)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,)
     async def session(self):
         async with self.session_factory() as sess:
             yield sess
