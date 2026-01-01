024706304f0ccc5802d0b2926cf4acb9c295d661
froriz556 <alex_fomof@mail.ru>
2026-01-02 00:46:34 +0300

    add sort to get all tasks func

--- api/todos/crud.py
+++ api/todos/crud.py
@@ -3,12 +3,15 @@ from typing import Annotated
 
 from fastapi import HTTPException, Path, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, text
 from sqlalchemy.engine import Result
 
 from api.todos.schemas import CreateTask, UpdateTask
 from core.models import db_helper
 from core.models.tasks import Task
+
+good_fields = ["created_at", "completed_at", "due_at", "completed"]
+
 
 async def create_task(session: AsyncSession, task_in: CreateTask):
     task = Task(**task_in.model_dump())
@@ -17,22 +20,40 @@ async def create_task(session: AsyncSession, task_in: CreateTask):
     await session.refresh(task)
     return task
 
-async def get_all_tasks(session: AsyncSession) -> list[Task]:
-    stmt = select(Task).order_by(Task.id)
+
+async def get_all_tasks(session: AsyncSession, order_by: str) -> list[Task]:
+    order_status = "ASC"
+    field = order_by
+    if order_by.startswith("-"):
+        field = order_by[1:]
+        order_status = "DESC"
+    if field in good_fields:
+        stmt = select(Task).order_by(text(f"{field} {order_status}"))
+    else:
+        raise HTTPException(status_code=400, detail="Invalid ordering category")
+
     result: Result = await session.execute(stmt)
     tasks = result.scalars().all()
     return list(tasks)
 
+
 async def get_task(session: AsyncSession, task_id: int):
     return await session.get(Task, task_id)
 
-async def get_task_by_id( task_id: Annotated[int, Path], session: AsyncSession = Depends(db_helper.session_dependency)):
+
+async def get_task_by_id(
+    task_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
     task = await get_task(session=session, task_id=task_id)
     if task is not None:
         return task
     raise HTTPException(status_code=404, detail=f"Task with id:{task_id} not found")
 
-async def patch_task(session: AsyncSession, update_task: UpdateTask, task: Task) -> Task:
+
+async def patch_task(
+    session: AsyncSession, update_task: UpdateTask, task: Task
+) -> Task:
     if update_task.completed:
         setattr(task, "completed_at", datetime.now())
     for k, v in update_task.model_dump(exclude_unset=True).items():
@@ -40,6 +61,7 @@ async def patch_task(session: AsyncSession, update_task: UpdateTask, task: Task)
     await session.commit()
     await session.refresh(task)
     return task
+
 
 async def patch_completed_task(session: AsyncSession, is_completed: bool, task: Task):
     setattr(task, "completed", is_completed)
@@ -49,7 +71,7 @@ async def patch_completed_task(session: AsyncSession, is_completed: bool, task: 
     await session.refresh(task)
     return task
 
+
 async def delete_task(session: AsyncSession, task: Task) -> None:
     await session.delete(task)
     await session.commit()
-
--- api/todos/views.py
+++ api/todos/views.py
@@ -6,28 +6,54 @@ from api.todos.schemas import CreateTask, UpdateTask, GetTask
 from core.models import Task
 from core.models.db_helper import db_helper
 from api.todos import crud
+
 router = APIRouter()
 
-@router.get("/")
-async def get_all_tasks(session: AsyncSession = Depends(db_helper.session_dependency)):
-    return await crud.get_all_tasks(session=session)
 
-@router.post("/")
-async def create_task(task: CreateTask, session: AsyncSession = Depends(db_helper.session_dependency)):
+@router.get("/")
+async def get_all_tasks(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    order_by: str = "created_at",
+):
+    return await crud.get_all_tasks(session=session, order_by=order_by)
+
+
+@router.post("/", status_code=201)
+async def create_task(
+    task: CreateTask, session: AsyncSession = Depends(db_helper.session_dependency)
+):
     return await crud.create_task(session=session, task_in=task)
 
+
 @router.get("/{task_id}")
-async def get_task(task_id: int, session: AsyncSession = Depends(db_helper.session_dependency)):
+async def get_task(
+    task_id: int, session: AsyncSession = Depends(db_helper.session_dependency)
+):
     return await crud.get_task_by_id(task_id=task_id, session=session)
 
+
 @router.patch("/{task_id}", response_model=GetTask)
-async def patch_task(update_task: UpdateTask, session: AsyncSession = Depends(db_helper.session_dependency), task: Task = Depends(get_task_by_id)):
+async def patch_task(
+    update_task: UpdateTask,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    task: Task = Depends(get_task_by_id),
+):
     return await crud.patch_task(session=session, update_task=update_task, task=task)
 
-@router.patch("/{task_id}/completed")
-async def patch_completed_task(session: AsyncSession = Depends(db_helper.session_dependency), task: Task = Depends(get_task_by_id)):
-    return await crud.patch_completed_task(session=session, task=task, is_completed=True)
+
+@router.post("/{task_id}/completed", status_code=201)
+async def patch_completed_task(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    task: Task = Depends(get_task_by_id),
+):
+    return await crud.patch_completed_task(
+        session=session, task=task, is_completed=True
+    )
+
 
 @router.delete("/{task_id}")
-async def delete_task(session: AsyncSession = Depends(db_helper.session_dependency), task: Task = Depends(get_task_by_id)) -> None:
+async def delete_task(
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    task: Task = Depends(get_task_by_id),
+) -> None:
     await crud.delete_task(session=session, task=task)
